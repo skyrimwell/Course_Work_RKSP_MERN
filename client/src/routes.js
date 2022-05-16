67cf01d2acfbd3cd94730933720d635512741feb
skyrimwell <skyrimwell@gmail.com>
2022-05-16 21:24:10 +0300

    IHOPETHISISLASTHEROKYDEPLOYTRY6

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import MainPage from './pages/mainpage/MainPage.jsx';
-import AuthPage from './pages/AuthPage/AuthPage.jsx';
+import MainPage from './pages/mainpage/MainPage';
+import AuthPage from './pages/AuthPage/AuthPage';
 //const MainPage = require("./pages/mainpage/MainPage.jsx")
 //const AuthPage = require("./pages/AuthPage/AuthPage.jsx")
 
