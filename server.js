09b6574ef6ca92d3d33854e134a45d584b9c3111
skyrimwell <skyrimwell@gmail.com>
2022-05-16 20:52:05 +0300

    IHOPETHISISLASTHEROKYDEPLOYTRY

--- server.js
+++ server.js
@@ -4,12 +4,26 @@ const dotenv = require("dotenv");
 
 const app = express()
 const PORT = app.listen(process.env.PORT || 5000)
-
+const path = require("path");
 app.use(express.json({extended: true}))
 app.use('/api/auth/', require('./routes/auth.route'))
 app.use('/api/notes', require('./routes/notes.route'))
 dotenv.config({ path: ".env" });
 const dbKey = process.env.REACT_APP_ACCESS_KEY;
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/client/build")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"client","build","index.html"))
+    })
+}else{
+    app.get("/", (req, res) => {
+        res.send("Api running")
+    })
+}
+
+
 async function start() {
 
     try {
